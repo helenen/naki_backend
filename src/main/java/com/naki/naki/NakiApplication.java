c40ac78556af855d1ba7ac6dd83e09caf9afeda0
helenen <helene.niassy@gmail.com>
2018-01-29 13:58:43 +0100

    add backend

---
+++ src/main/java/com/naki/naki/NakiApplication.java
@@ -0,0 +1,12 @@
+package com.naki.naki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NakiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/naki/naki/NakiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naki.naki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NakiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
