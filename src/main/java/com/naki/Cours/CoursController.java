13e633e9677035bb6f6881c221180e0e2b920a49
helenen <helene.niassy@gmail.com>
2018-06-04 23:55:11 +0200

    merdasse 2

--- src/main/java/com/naki/Cours/CoursController.java
+++ src/main/java/com/naki/Cours/CoursController.java
@@ -1,11 +1,15 @@
 package com.naki.Cours;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@RequestMapping("/api")
 @RestController
 public class CoursController {
 
--- src/main/java/com/naki/Lesson/LessonController.java
+++ src/main/java/com/naki/Lesson/LessonController.java
@@ -3,11 +3,13 @@ package com.naki.Lesson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+
+@RequestMapping("/api")
 @RestController
 public class LessonController {
 
